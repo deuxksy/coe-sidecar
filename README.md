92f7002284a581a3ecefe8d82515c29e99578d96
lamos kim <boston@Act-Boston.local>
2018-06-05 14:43:59 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-sidecar example
+# Sidecar pattern example
+
+- node-service : simple non JVM microservice
+- sidecar : spring cloud netflix sidecar example
+
+## 구성도
+
+![sidecar-pattern](./document/images/sidecar-pattern.png)
+
+## 실행 방법
+1. node-service 설치 및 실행
+    ```bash
+    > cd node-service
+    > npm install
+    > node index.js
+    ```
+    간단한 express 서버가 8008포트로 동작합니다.
+2. sidecar 설치 및 실행
+    ```bash
+    > mvn clean install
+    > java -jar ./target/sidecar-0.0.1.SNAPSHOT.jar
+    ```
+    sidecar 예제 프로그램이 8009포트로 동작합니다.
+
+
+## 동작
+1. discovery sidecar application
+    - Eureka에 등록된 serviceId(NODE-SERVICE)로 사용 가능
+![discovery-node-server](./document/images/discovery-node-server.png)
+    - 예제코드
+![discovery-sample-code](./document/images/discovery-sample-code.png)
+
+2. discovery another application
+    - **sidecarHostName** : **sidecarPort** / **servceId**
+    - serviceId를 이용하여 Node Server에서 다른 micro service 사용 가능
+    - Node server에서 http://localhost:8009/Service1/someEndPoint 를 접근하는 경우 sidecar application이 Eureka서버에서 서버 정보를 가져와 Service1으로 라우팅 해준다.
+![discovery-another-service](./document/images/discovery-another-service.png) 
--- sidecar/src/main/java/com/coe/sidecar/SidecarApplication.java
+++ sidecar/src/main/java/com/coe/sidecar/SidecarApplication.java
@@ -1,10 +1,13 @@
 package com.coe.sidecar;
 
+import com.oracle.tools.packager.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 import org.springframework.context.annotation.Bean;
@@ -17,22 +20,23 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class SidecarApplication {
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate;
-    }
-
-    @Autowired
-    RestTemplate restTemplate;
-
     public static void main(String[] args) {
         SpringApplication.run(SidecarApplication.class, args);
     }
 
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
     @RequestMapping("/hi")
     public String hello() {
-        return restTemplate.getForObject("http://node-service/hello", String.class);
+
+        // Eureka서버에서 node-service의 서버정보를 가져온다.
+        ServiceInstance serviceInstance = loadBalancerClient.choose("node-service");
+
+        String hostName = serviceInstance.getHost();    // localhost
+        int port = serviceInstance.getPort();           // 8008
+
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("http://" + hostName + ":" + port + "/hello", String.class);
     }
 }
