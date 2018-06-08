d5baabe2d698a871505056a828eb76d9de839cff
lamos KIM <act.lamos@gmail.com>
2018-06-08 18:38:09 +0900

    modify readme.md and remove unused import

--- sidecar/src/main/java/com/coe/sidecar/SidecarApplication.java
+++ sidecar/src/main/java/com/coe/sidecar/SidecarApplication.java
@@ -1,16 +1,11 @@
 package com.coe.sidecar;
 
-import com.oracle.tools.packager.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
